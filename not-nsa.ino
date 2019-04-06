0bd7c62fa5d763c2c9753906a21f58eda0e8de07
Christoffer <chsnot@gmail.com>
2019-04-06 19:16:44 +0200

    Add event checking

--- not-nsa.ino
+++ not-nsa.ino
@@ -14,8 +14,16 @@ struct User {
     char liu_id[9];
 };
 
+struct Event_attendee {
+  char liu_id[9];
+  String note;
+  bool logged = false;
+};
+
 int user_count = 0;
+int event_count = 0;
 User users[30];
+Event_attendee events_attendees[100];
 
 uint8_t last_uid[UID_LENGTH];
 
@@ -41,6 +49,12 @@ void setup() {
     // users[1].uid = [0xB4, 0x25, 0xD9, 0x1E];
     // users[1].liu_id = "yoloo420";
     user_count = 2;
+
+    strcpy(events_attendees[0].liu_id, "frask812");
+    events_attendees[0].note = " hej, jag tål inte emacs";
+    event_count = 1;
+
+    
 }
 
 
@@ -55,7 +69,7 @@ void loop() {
             for(size_t i = 0; i < user_count; i++) {
                 if(memcmp(rfid.uid.uidByte, users[i].uid, UID_LENGTH) == 0) {
                     Serial.println(users[i].liu_id);
-
+                    check_event(users[i].liu_id);
                     recognised_card = true;
                 }
             }
@@ -102,4 +116,25 @@ void dump_byte_array(byte *buffer, byte bufferSize) {
         Serial.print(buffer[i] < 0x10 ? " 0" : " ");
         Serial.print(buffer[i], HEX);
     }
+}
+
+void check_event(char* liu_id) {
+      bool found = false;
+      for(int i = 0; i < event_count; i++) {
+          if(strcmp(liu_id, events_attendees[i].liu_id) == 0) {
+                if(!events_attendees[i].logged){
+                    events_attendees[i].logged = true;
+                    Serial.print(liu_id);
+                    Serial.println(events_attendees[i].note);
+                    found = true;  
+                } else {
+                    Serial.println("Allready logged");
+                    found = true;
+                }
+                
+          }
+      }
+      if(!found){
+          Serial.println("Not registered for this event!");
+      }
 }
