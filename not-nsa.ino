335b0c7fdd09100bef8acb6ec466125f3406b7b3
TheZoq2 <frans.skarman@gmail.com>
2019-04-06 13:16:42 +0200

    Add reading of nfc cards

--- not-nsa.ino
+++ not-nsa.ino
@@ -1,46 +1,51 @@
-/*
-        This sketch demonstrates how to scan WiFi networks.
-        The API is almost the same as with the WiFi Shield library,
-        the most obvious difference being the different file you need to include:
-*/
-#include "ESP8266WiFi.h"
+#include <SPI.h>
+#include <MFRC522.h>
+
+const int RST_PIN = 16;
+const int SS_PIN = 2;
+
+MFRC522 rfid(SS_PIN, RST_PIN);
+
+struct User {
+    uint8_t[5] hash;
+    uint8_t[8] liu_id;
+}
 
 void setup() {
     Serial.begin(115200);
-
-    // Set WiFi to station mode and disconnect from an AP if it was previously connected
-    WiFi.mode(WIFI_STA);
-    WiFi.disconnect();
-    delay(100);
-
-    Serial.println("Setup done");
+    SPI.begin();
+    rfid.PCD_Init();
+    rfid.PCD_DumpVersionToSerial();
+    Serial.println("Done");
 }
 
 void loop() {
-    Serial.println("scan start");
+    int user_count = 0;
+    User users[30];
 
-    // WiFi.scanNetworks will return the number of networks found
-    int n = WiFi.scanNetworks();
-    Serial.println("scan done");
-    if (n == 0) {
-        Serial.println("no networks found");
-    } else {
-        Serial.print(n);
-        Serial.println(" networks found");
-        for (int i = 0; i < n; ++i) {
-            // Print SSID and RSSI for each network found
-            Serial.print(i + 1);
-            Serial.print(": ");
-            Serial.print(WiFi.SSID(i));
-            Serial.print(" (");
-            Serial.print(WiFi.RSSI(i));
-            Serial.print(")");
-            Serial.println((WiFi.encryptionType(i) == ENC_TYPE_NONE) ? " " : "*");
-            delay(10);
+    while(true) {
+        // Reset the loop if no new card present on the sensor/reader. This saves
+        // the entire process when idle.
+        if ( ! rfid.PICC_IsNewCardPresent()) {
+            break;
         }
-    }
-    Serial.println("");
 
-    // Wait a bit before scanning again
-    delay(5000);
+        // Select one of the cards
+        if ( ! rfid.PICC_ReadCardSerial()) {
+            break;
+        }
+
+        // Dump debug info about the card; PICC_HaltA() is automatically called
+        // rfid.PICC_DumpToSerial(&(rfid.uid));
+
+        dump_byte_array(rfid.uid.uidByte, rfid.uid.size);
+        Serial.println("");
+    }
+}
+
+void dump_byte_array(byte *buffer, byte bufferSize) {
+    for (byte i = 0; i < bufferSize; i++) {
+        Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+        Serial.print(buffer[i], HEX);
+    }
 }
